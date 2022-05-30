36ecf3265fabe76df311917835aa3e57e88cdce5
sanuksd <100213849+sanuksd@users.noreply.github.com>
2022-05-30 17:19:14 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -129,7 +129,7 @@ async def next_page(bot, query):
         )
     elif off_set is None:
         btn.append([InlineKeyboardButton(f"📝 {round(int(offset)/10)+1} / {round(total/10)}", callback_data="pages"), InlineKeyboardButton("ɴᴇxᴛ 🚸", callback_data=f"next_{req}_{key}_{n_offset}")]
-    elif btn:
+    elif:
         btn.append([InlineKeyboardButton("🔰 join my movie team 🔰",url="https://t.me/+Qb_WFOWoi3VkYTEx")])
     else:
         btn.append(

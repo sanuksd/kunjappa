eeff2653eeb856b343a139ff7f4c3c7cc756e7d8
sanuksd <100213849+sanuksd@users.noreply.github.com>
2022-06-03 08:47:27 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -669,7 +669,7 @@ async def auto_filter(client, msg, spoll=False):
         )
     imdb = await get_poster(search, file=(files[0]).file_name) if IMDB else None
     if imdb:
-        cap = IMDB_TEMPLATE.format(
+        cap = TEMPLATE.format(
             query = search,
             title = imdb['title'],
             votes = imdb['votes'],

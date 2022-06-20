5f13e2887b92db1080c37ca2cc0979a09b2b33ca
sanuksd <100213849+sanuksd@users.noreply.github.com>
2022-06-20 19:50:13 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -91,7 +91,7 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_sanufilterv1_bot=query)
+            InlineKeyboardButton('Easy way to find movie', url=https://t.me/sanusmovies)
         ]
         ]
     return InlineKeyboardMarkup(buttons)
